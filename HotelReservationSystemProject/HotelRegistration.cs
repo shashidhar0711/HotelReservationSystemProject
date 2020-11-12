66e79ea6ca45b9a3b98cb55e8fe2d22d08b5f69d
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-12 23:53:02 +0530

    [sasic] Add . Added WeekDay Rates and WeekEnd Rates of Hotel

---
+++ HotelReservationSystemProject/HotelRegistration.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystemProject
+{
+    public class HotelRegistration
+    {
+        /// <summary>
+        /// defining hotel list and dictionary
+        /// </summary>
+        List<Hotel> hotelList = new List<Hotel>();
+        SortedDictionary<int, string> sortingCost = new SortedDictionary<int, string>();
+
+        /// <summary>
+        /// Adds the hotel.
+        /// </summary>
+        public void AddHotel()
+        {
+            /// Adding hotel details in to hotel list
+            hotelList.Add(new Hotel("LakeWood", 110, 90));
+            hotelList.Add(new Hotel("BridgeWood", 160, 60));
+            hotelList.Add(new Hotel("RidgeWood", 220, 150));
+
+            /// Iterating through list
+            foreach (var hotels in hotelList)
+            {
+                Console.WriteLine(hotels.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Calculatings the hotel prices.
+        /// </summary>
+        /// <param name="startingDate">The starting date.</param>
+        /// <param name="endingDate">The ending date.</param>
+        public void calculatingHotelPrices(DateTime startingDate, DateTime endingDate)
+        {
+            int totalCost = 0;
+            TimeSpan DateDifference = endingDate.Subtract(startingDate);
+            foreach (var hotels in hotelList)
+            {
+                /// By looping untill Condtion ends
+                for (int i = 0; i < DateDifference.TotalDays; i++)
+                {
+                    totalCost = totalCost + hotels.weekDayRegularRates;
+                }
+                Console.WriteLine("Hotel Name : " + hotels.hotelName, "Total Price : " + totalCost);
+                /// Adding key and values in to dictionary
+                sortingCost.Add(totalCost, hotels.hotelName);
+            }
+        }
+        /// <summary>
+        /// cheapestHotel method is used to find the cheapest hotel among the present hotels.
+        /// </summary>
+        /// <param name="checkInDate"></param>
+        /// <param name="checkOutDate"></param>
+        public void FindingCheapestHotel(DateTime startingDate, DateTime endingDate)
+        {
+            calculatingHotelPrices(startingDate, endingDate);
+            foreach (var hotelPrice in sortingCost)
+            {
+                /// In sorted dictionary should get cheapest
+                Console.WriteLine("Cheapest Hotel Name : " + hotelPrice.Value, "Cheapest Cost : " + hotelPrice.Key);
+                break;
+            }
+        }
+    }
+}
--- HotelReservationSystemProject/Program.cs
+++ HotelReservationSystemProject/Program.cs
@@ -2,11 +2,27 @@
 
 namespace HotelReservationSystemProject
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welocme to Hotel Registration");
+            HotelRegistration hotelRegistration = new HotelRegistration();
+            hotelRegistration.AddHotel();
+
+            /// Taking starting and ending date from console and,
+            /// to find difference and cheapest hotel
+            Console.WriteLine("Enter the checkDate from start date ddmmyyyy");
+            string checkFromDate = Console.ReadLine();
+            Console.WriteLine("Enter the checkDate to date ddmmyyyy");
+            string checkToDate = Console.ReadLine();
+            DateTime startingDate = DateTime.Parse(checkFromDate);
+            DateTime endingDate = DateTime.Parse(checkToDate);
+            hotelRegistration.FindingCheapestHotel(startingDate, endingDate);
         }
     }
 }
