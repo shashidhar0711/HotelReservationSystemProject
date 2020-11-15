b10c18bbe47f92baa031ce37955898d360e03352
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-15 19:17:24 +0530

    [sasic] Add . Added WeekDays and WeekEnd Hotel Rewards Rates

---
+++ HotelReservationSystemProject/Hotel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystemProject
+{
+    public class Hotel
+    {
+        /// <summary>
+        /// Variables
+        /// </summary>
+        public string hotelName;
+        public int weekDayRegularRates;
+        public int weekEndRegularRates;
+        public int rating;
+        public int totalCost;
+        public int weekDayRewardsRates;
+        public int weekEndRewardsRates;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Hotel"/> class.
+        /// </summary>
+        /// <param name="hotelName">Name of the hotel.</param>
+        /// <param name="weekDayRegularRates">The week day regular rates.</param>
+        /// <param name="weekEndRegularRates">The week end regular rates.</param>
+        /// <param name="rating">The rating.</param>
+        public Hotel(string hotelName, int weekDayRegularRates, int weekEndRegularRates, int rating)
+        {
+            this.hotelName = hotelName;
+            this.weekDayRegularRates = weekDayRegularRates;
+            this.weekEndRegularRates = weekEndRegularRates;
+            this.rating = rating;
+        }
+
+        public Hotel(int totalCost, string hotelName, int rating)
+        {
+            this.totalCost = totalCost;
+            this.hotelName = hotelName;
+            this.rating = rating;
+
+        }
+
+        public Hotel(string hotelName, int weekDayRewardsRates, int weekEndRewardsRates)
+        {
+            this.hotelName = hotelName;
+            this.weekDayRewardsRates = weekDayRewardsRates;
+            this.weekEndRewardsRates = weekEndRewardsRates;
+        }
+
+        /// <summary>
+        /// Sets the name of the hotel.
+        /// </summary>
+        /// <param name="hotelName">Name of the hotel.</param>
+        public void setHotelName(string hotelName)
+        {
+            this.hotelName = hotelName;
+        }
+
+        /// <summary>
+        /// Sets the week day regular rates.
+        /// </summary>
+        /// <param name="weekDayRegularRates">The week day regular rates.</param>
+        public void setWeekDayRegularRates(int weekDayRegularRates)
+        {
+            this.weekDayRegularRates = weekDayRegularRates;
+        }
+
+        /// <summary>
+        /// Sets the week end regular rates.
+        /// </summary>
+        /// <param name="weekEndRegularRates">The week end regular rates.</param>
+        public void SetWeekEndRegularRates(int weekEndRegularRates)
+        {
+            this.weekEndRegularRates = weekEndRegularRates;
+        }
+
+        /// <summary>
+        /// Sets the rating.
+        /// </summary>
+        /// <param name="rating">The rating.</param>
+        public void SetRating(int rating)
+        {
+            this.rating = rating;
+        }
+
+        /// <summary>
+        /// Gets the name of the hotel.
+        /// </summary>
+        /// <returns></returns>
+        public string GetHotelName()
+        {
+            return hotelName;
+        }
+
+        /// <summary>
+        /// Gets the week day regular rates.
+        /// </summary>
+        /// <returns></returns>
+        public int GetWeekDayRegularRates()
+        {
+            return weekDayRegularRates;
+        }
+
+        /// <summary>
+        /// Gets the week end regular rates.
+        /// </summary>
+        /// <returns></returns>
+        public int GetWeekEndRegularRates()
+        {
+            return weekEndRegularRates;
+        }
+
+        /// <summary>
+        /// Gets the rating.
+        /// </summary>
+        /// <returns></returns>
+        public int GetRating()
+        {
+            return rating;
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        override
+        public string ToString()
+        {
+            return "hotelName : " + hotelName + " || " + "WeekDayRegularRates : " + weekDayRegularRates + " || " + "WeekEndRegularRates : " + weekEndRegularRates + " || " + "HotelRating : " + rating;
+        }
+    }
+}
