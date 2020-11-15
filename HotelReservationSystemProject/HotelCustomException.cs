7c2578d9d6271be810a5a67fb4e46950fbeb0ca8
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-15 22:38:23 +0530

    [sasic] Add . Added To Find Cheapest Hotel Rewards Cost

---
+++ HotelReservationSystemProject/HotelCustomException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystemProject
+{
+    public class HotelCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_CUSTOMER, VALID_DATE_FORMAT
+        }
+
+        /// <summary>
+        /// The type
+        /// </summary>
+        public ExceptionType type;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HotelCustomException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="type">The type.</param>
+        public HotelCustomException(string message, ExceptionType type) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
--- HotelReservationSystemProject/Program.cs
+++ HotelReservationSystemProject/Program.cs
@@ -2,11 +2,31 @@
 
 namespace HotelReservationSystemProject
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welocme to Hotel Registration");
+            HotelReservation hotelRegistration = new HotelReservation();
+            Console.WriteLine("Enter the type of Customer(Regular/Rewards)");
+            string customerType = Console.ReadLine();
+            hotelRegistration.AddHotel(customerType.ToLower());
+
+            /// Taking starting and ending date from console and,
+            /// to find difference and cheapest hotel
+            Console.WriteLine("Enter the checkDate from start date ddmmyyyy");
+            string checkFromDate = Console.ReadLine();
+            Console.WriteLine("Enter the checkDate to date ddmmyyyy");
+            string checkToDate = Console.ReadLine();
+            Console.WriteLine("Enter Check-In day");
+            string checkInDay = Console.ReadLine();
+            DateTime startingDate = DateTime.Parse(checkFromDate);
+            DateTime endingDate = DateTime.Parse(checkToDate);
+            hotelRegistration.FindCheapestHotelWithRatings(startingDate, endingDate, checkInDay.ToLower());
         }
     }
 }
